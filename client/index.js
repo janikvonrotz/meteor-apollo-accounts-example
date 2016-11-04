10ac079f38c66a3ccd3e741cdc88ba1dfc9e6372
Janik von Rotz <login@janikvonrotz.ch>
2016-11-04 15:18:01 +0100

    eslint init

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import App from './App'
+import Layout from './Layout'
+import Login from './Login'
+import RecoverPassword from './Password'
+import Register from 'Register'
+
+export { App, Layout, Login, RecoverPassword, Register }
